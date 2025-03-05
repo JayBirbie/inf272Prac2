44cfc660a290f4fed9ab41ed239c6b949e698b2f
Jaybie <neli.mashele@gmail.com>
2025-03-05 18:23:48 +0200

    Add project files.

---
+++ JayStudyGroupProfiles/Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JayStudyGroupProfiles.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult List()
+        {
+            List<Models.PersonModel> members = new List<Models.PersonModel>();
+
+            members.Add(new Models.PersonModel { stuNum = "u20467207", fName = "Raelene", lName = "Dookkoo", email = "raelene.d@tuks.co.za", myLink = "../HTML/raePage.html"});
+            members.Add(new Models.PersonModel { stuNum = "u23854830", fName = "Dakalo", lName = "Nemamilwe", email = "u23854830@tuks.co.za" });
+            members.Add(new Models.PersonModel { stuNum = "u22563742", fName = "Boikgantsho", lName = "More", email = "22563742@tuks.co.za" });
+            members.Add(new Models.PersonModel { stuNum = "u22500333", fName = "Jay", lName = "Mashele", email = "njl.mashele@tuks.co.za" });
+            members.Add(new Models.PersonModel { stuNum = "u24596385", fName = "Ryan", lName = "van Wyk", email = "r.vanwyk@tuks.co.za" });
+
+            return View(members);
+        }
+    }
+}
---
+++ JayStudyGroupProfiles/Models/PersonModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JayStudyGroupProfiles.Models
+{
+    public class PersonModel
+    { 
+ 
+        [Display(Name = "Student Number")]
+        public string stuNum { get; set; }
+
+        [Display(Name = "First Name")]
+        public string fName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string lName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string email { get; set; }
+
+        [Display(Name = "Link to Personal Page")]
+        public string myLink { get; set; }
+    }
+}
