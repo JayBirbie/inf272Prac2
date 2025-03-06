ac5aa5bf1c39f85258b8c2c446610c70b1796190
JayBirbie <neli.mashele@gmail.com>
2025-03-06 09:47:10 +0200

    button achived

--- JayStudyGroupProfiles/Controllers/PeopleController.cs
+++ JayStudyGroupProfiles/Controllers/PeopleController.cs
@@ -13,11 +13,11 @@ namespace JayStudyGroupProfiles.Controllers
         {
             List<Models.PersonModel> members = new List<Models.PersonModel>();
 
-            members.Add(new Models.PersonModel { stuNum = "u20467207", fName = "Raelene", lName = "Dookkoo", email = "raelene.d@tuks.co.za", myLink = "/HTML/raePage.html"});
-            members.Add(new Models.PersonModel { stuNum = "u23854830", fName = "Dakalo", lName = "Nemamilwe", email = "u23854830@tuks.co.za" });
-            members.Add(new Models.PersonModel { stuNum = "u22563742", fName = "Boikgantsho", lName = "More", email = "22563742@tuks.co.za" });
-            members.Add(new Models.PersonModel { stuNum = "u22500333", fName = "Jay", lName = "Mashele", email = "njl.mashele@tuks.co.za" });
-            members.Add(new Models.PersonModel { stuNum = "u24596385", fName = "Ryan", lName = "van Wyk", email = "r.vanwyk@tuks.co.za" });
+            members.Add(new Models.PersonModel { stuNum = "u20467207", fName = "Raelene", lName = "Dookkoo", email = "raelene.d@tuks.co.za", myLink = "../HTML/raePage.html"});
+            members.Add(new Models.PersonModel { stuNum = "u23854830", fName = "Dakalo", lName = "Nemamilwe", email = "u23854830@tuks.co.za", myLink = "../HTML/duckyPage.html" });
+            members.Add(new Models.PersonModel { stuNum = "u23959094", fName = "Jessica", lName = "Turner", email = "u23959094@tuks.co.za", myLink = "../HTML/jessPage.html" });
+            members.Add(new Models.PersonModel { stuNum = "u22500333", fName = "Jay", lName = "Mashele", email = "njl.mashele@tuks.co.za", myLink = "../HTML/jayPage.html" });
+            members.Add(new Models.PersonModel { stuNum = "u24596385", fName = "Ryan", lName = "van Wyk", email = "r.vanwyk@tuks.co.za", myLink = "../HTML/ryanPage.html" });
 
             return View(members);
         }
